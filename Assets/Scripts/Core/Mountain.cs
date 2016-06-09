2463bec552a974aabce350985528583d0491a398
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-09 19:01:01 +0545

    Add MountainsParser to parse mountain objects from Mountains.json resource

---
+++ Assets/Scripts/Core/Mountain.cs
@@ -0,0 +1,33 @@
+﻿namespace TheHimalayas.Core {
+
+    public class Mountain {
+
+        /// <summary>
+        /// 
+        /// Name of the Mountain
+        /// 
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// 
+        /// Elevation of the Mountain
+        /// 
+        /// </summary>
+        public int elevation;
+
+        /// <summary>
+        /// 
+        /// Wikipedia Title of the Mountain to fetch Information about the mountain
+        /// 
+        /// </summary>
+        public string wikipediaTitle;
+
+        /// <summary>
+        /// 
+        /// Coordinates of the Moutain
+        /// 
+        /// </summary>
+        public Location coordinates;
+    }
+}
---
+++ Assets/Scripts/Parsers/MountainsParser.cs
@@ -0,0 +1,90 @@
+﻿using TheHimalayas.Core;
+using System.Collections.Generic;
+using SimpleJSON;
+
+namespace TheHimalayas.Parsers {
+
+    public class MountainsParser : IJsonDataParser<Dictionary<string,Mountain>> {
+
+        /// <summary>
+        /// 
+        /// Parses a JSON to get dictionary of Mountains
+        /// 
+        /// </summary>
+        /// <param name="json">JSON String to parse</param>
+        /// <returns>Dictionary of Mountains parsed from the JSON string</returns>
+        public Dictionary<string, Mountain> Parse(string json) {
+            JSONNode node = JSON.Parse(json);
+
+            Dictionary<string, Mountain> mountains = new Dictionary<string, Mountain>();
+
+            foreach(JSONNode mountainJson in node.Children) {
+                mountains[mountainJson["key"]] = ParseMountain(mountainJson);
+            }
+
+            return mountains;
+        }
+
+        /// <summary>
+        /// 
+        /// Parses a single mountain from JSON
+        /// 
+        /// </summary>
+        /// <param name="mountainJson">JSON to be parsed.</param>
+        /// <returns>Parsed Mountain</returns>
+        private static Mountain ParseMountain(JSONNode mountainJson) {
+            Mountain mountain = new Mountain();
+
+            mountain.name = ParseMountainName(mountainJson);
+            mountain.elevation = ParseMountainElevation(mountainJson);
+            mountain.wikipediaTitle = ParseMountainWikipediaTitle(mountainJson);
+            mountain.coordinates = ParseMountainLocation(mountainJson);
+
+            return mountain;
+        }
+
+        /// <summary>
+        /// 
+        /// Parse Mountain Name
+        /// 
+        /// </summary>
+        /// <param name="mountainJson">JSON to be parsed</param>
+        /// <returns>Parsed Mountain Name</returns>
+        private static JSONNode ParseMountainName(JSONNode mountainJson) {
+            return mountainJson["name"];
+        }
+
+        /// <summary>
+        /// 
+        /// Parse Mountain Elevation
+        /// 
+        /// </summary>
+        /// <param name="mountainJson">JSON to be parsed</param>
+        /// <returns>Parsed Mountain Elevation</returns>
+        private static int ParseMountainElevation(JSONNode mountainJson) {
+            return mountainJson["elevation"].AsInt;
+        }
+
+        /// <summary>
+        /// 
+        /// Parse Mountain Wikipedia Title
+        /// 
+        /// </summary>
+        /// <param name="mountainJson">JSON to be parsed</param>
+        /// <returns>Parsed Mountain Wikipedia Title</returns>
+        private static JSONNode ParseMountainWikipediaTitle(JSONNode mountainJson) {
+            return mountainJson["wikipedia_title"];
+        }
+
+        /// <summary>
+        /// 
+        /// Parse Mountain Location
+        /// 
+        /// </summary>
+        /// <param name="mountainJson">JSON to be parsed</param>
+        /// <returns>Parsed Mountain Location</returns>
+        private static Location ParseMountainLocation(JSONNode mountainJson) {
+            return new Location(mountainJson["coordinates"]["latitude"].AsFloat, mountainJson["coordinates"]["longitude"].AsFloat);
+        }
+    }
+}
