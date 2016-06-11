8a1325b45f3da1cde8166c0ca485e81f75ec7136
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-11 22:27:36 +0545

    Add MenuUIManager and MountainSelectorUI to load Mountain3DScene from MenuScene

---
+++ Assets/Scripts/UI/MenuUIManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TheHimalayas.UI {
+
+    public class MenuUIManager : MonoBehaviour {
+
+        /// <summary>
+        /// 
+        /// Mountain Selector UI object
+        /// 
+        /// </summary>
+        public MountainSelectorUI mountainSelector;
+
+        /// <summary>
+        /// 
+        /// Mountain Locator Button
+        /// 
+        /// </summary>
+        public Button mountainLocatorBtn;
+
+        // When the script enables
+        void OnEnable() {
+            mountainSelector.viewMountainBtn.onClick.AddListener(HideInteractables);
+        }
+
+        // When the script disables
+        void OnDisable() {
+            mountainSelector.viewMountainBtn.onClick.RemoveListener(HideInteractables);
+        }
+
+        /// <summary>
+        /// 
+        /// Hides Interactable elements from UI
+        /// 
+        /// </summary>
+        private void HideInteractables() {
+            mountainSelector.HideInteractables();
+            mountainLocatorBtn.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MountainSelectorUI.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace TheHimalayas.UI {
+
+    public class MountainSelectorUI : MonoBehaviour {
+
+        /// <summary>
+        /// 
+        /// Mountain Text
+        /// 
+        /// </summary>
+        public Text mountainText;
+
+        /// <summary>
+        /// 
+        /// Previous Mountain Button
+        /// 
+        /// </summary>
+        public Button previousMountainBtn;
+
+        /// <summary>
+        /// 
+        /// Next Mountain Button
+        /// 
+        /// </summary>
+        public Button nextMountainBtn;
+
+        /// <summary>
+        /// 
+        /// View Mountain Button
+        /// 
+        /// </summary>
+        public Button viewMountainBtn;
+
+        // When the script enables
+        void OnEnable() {
+            viewMountainBtn.onClick.AddListener(LoadMountain3DScene);
+        }
+
+        // When the script disables
+        void OnDisable() {
+            viewMountainBtn.onClick.RemoveListener(LoadMountain3DScene);
+        }
+
+        /// <summary>
+        /// 
+        /// Loads Mountain 3D Scene
+        /// 
+        /// </summary>
+        private void LoadMountain3DScene() {
+            HideInteractables();
+            UpdateMountainText("Pushing Lands...");
+
+            SceneManager.LoadSceneAsync("Mountain3DScene");
+        }
+
+        /// <summary>
+        /// 
+        /// Updates the Mountain Text
+        /// 
+        /// </summary>
+        /// <param name="text">Text to be updated</param>
+        public void UpdateMountainText(string text) {
+            mountainText.text = text;
+        }
+
+        /// <summary>
+        /// 
+        /// Hides Interactable Buttons on the UI
+        /// 
+        /// </summary>
+        public void HideInteractables() {
+            previousMountainBtn.gameObject.SetActive(false);
+            nextMountainBtn.gameObject.SetActive(false);
+            viewMountainBtn.gameObject.SetActive(false);
+        }
+    }
+}
