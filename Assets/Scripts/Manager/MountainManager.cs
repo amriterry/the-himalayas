96723e12b2a0d671297b06c9bffb88b17da401fe
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-13 21:52:34 +0545

    Make MountainManager a singleton in the application

--- Assets/Scripts/Manager/MountainManager.cs
+++ Assets/Scripts/Manager/MountainManager.cs
@@ -13,8 +13,42 @@ namespace TheHimalayas.Manager {
         /// </summary>
         private MountainFactory factory;
 
+        /// <summary>
+        /// 
+        /// AppEngine Instance
+        /// 
+        /// </summary>
+        private static MountainManager instance;
+
+        /// <summary>
+        /// 
+        /// AppEngine Singleton property
+        /// 
+        /// </summary>
+        public static MountainManager Instance {
+            get {
+                if (instance == null) { 
+                    instance = GameObject.FindObjectOfType<MountainManager>();
+                }
+
+                MountainManager[] controllers = GameObject.FindObjectsOfType<MountainManager>();
+
+                if (controllers.Length > 1) {
+                    foreach (MountainManager controller in controllers) {
+                        if (controller != instance) {
+                            Destroy(controller.gameObject);
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
         // Use this for initialization
         void Awake() {
+            DontDestroyOnLoad(gameObject);
+
             factory = GetComponent<MountainFactory>();
 
             AppEngine.Instance.GetMountainStore().InitailizeStore(factory.GetMountains());
--- Assets/Scripts/UI/MountainWeatherUIManager.cs
+++ Assets/Scripts/UI/MountainWeatherUIManager.cs
@@ -6,43 +6,119 @@ namespace TheHimalayas.UI {
 
     public class MountainWeatherUIManager : MonoBehaviour {
 
+        /// <summary>
+        /// 
+        /// Array of Mountain Texts
+        /// 
+        /// </summary>
         public Text[] mountainTexts;
 
+        /// <summary>
+        /// 
+        /// Weather status text
+        /// 
+        /// </summary>
         public Text weatherStatusText;
 
+        /// <summary>
+        /// 
+        /// Location of mountain text
+        /// 
+        /// </summary>
         public Text locationText;
 
+        /// <summary>
+        /// 
+        /// Weather text
+        ///  
+        /// </summary>
         public Text weatherText;
 
+        /// <summary>
+        /// 
+        /// Corrdinates of mountain text
+        /// 
+        /// </summary>
         public Text coordinatesText;
 
+        /// <summary>
+        /// 
+        /// Temperature text
+        /// 
+        /// </summary>
         public Text temperatureText;
 
+        /// <summary>
+        /// 
+        /// Minimum and Maximum Temperature text
+        /// 
+        /// </summary>
         public Text minMaxTempText;
 
+        /// <summary>
+        /// 
+        /// Pressure Text
+        /// 
+        /// </summary>
         public Text pressureText;
 
+        /// <summary>
+        /// 
+        /// Cloudiness text
+        /// 
+        /// </summary>
         public Text cloudinessText;
 
+        /// <summary>
+        /// 
+        /// Last Data Calculation Time text
+        /// 
+        /// </summary>
         public Text dctText;
 
+        /// <summary>
+        /// 
+        /// Detail Weather panel
+        /// 
+        /// </summary>
         public GameObject detailWeatherPanel;
 
-
+        /// <summary>
+        /// 
+        /// Opens Detail Weather Panel
+        /// 
+        /// </summary>
         public void OpenDetailWeatherPanel() {
             detailWeatherPanel.SetActive(true);
         }
 
+        /// <summary>
+        /// 
+        /// Closes Detail Weather Panel
+        /// 
+        /// </summary>
         public void CloseDetailWeatherPanel() {
             detailWeatherPanel.SetActive(false);
         }
 
+        /// <summary>
+        /// 
+        /// Updates Mountain Texts
+        /// 
+        /// </summary>
+        /// <param name="mountain">Mountain for which the mountain text is to be updated</param>
         public void UpdateMountainTexts(Mountain mountain) {
             foreach(Text t in mountainTexts) {
                 t.text = mountain.name;
             }
         }
 
+        /// <summary>
+        /// 
+        /// Updates Weather information in UI for the mountain
+        /// 
+        /// </summary>
+        /// <param name="weather">Weather information which is to be updated</param>
         public void UpdateMountainWeather(Weather weather) {
             weatherStatusText.text = "weather: at " + weather.temperature.ToCelciusString() + " in '" + weather.cityName + "'";
 
