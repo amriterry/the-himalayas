d08f068de7a6d382182fca15c537f7adf56d3c24
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-14 05:57:45 +0545

    Add Forecast Http Client and URL Builder

---
+++ Assets/Scripts/Http/ForecastHttpClient.cs
@@ -0,0 +1,29 @@
+﻿using TheHimalayas.Http.Url;
+
+namespace TheHimalayas.Http {
+
+    public class ForecastHttpClient : OpenWeatherHttpClient {
+
+        /// <summary>
+        /// 
+        /// Builds the URL for forecast
+        /// 
+        /// </summary>
+        private ForecastUrlBuilder urlBuilder;
+
+        // When the script first awakens
+        void Awake() {
+            urlBuilder = new ForecastUrlBuilder();
+        }
+
+        /// <summary>
+        /// 
+        /// Returns OpenWeatherUrlBuilder object
+        /// 
+        /// </summary>
+        /// <returns>OpenWeatherUrlBuilder object</returns>
+        protected override OpenWeatherUrlBuilder GetUrlBuilder() {
+            return urlBuilder;
+        }
+    }
+}
---
+++ Assets/Scripts/Http/Url/ForecastUrlBuilder.cs
@@ -0,0 +1,39 @@
+﻿using TheHimalayas.Core;
+
+namespace TheHimalayas.Http.Url {
+
+    public class ForecastUrlBuilder : OpenWeatherUrlBuilder {
+
+        /// <summary>
+        /// 
+        /// Number of Forecasts for which the URL is to be built
+        /// 
+        /// </summary>
+        private const int FORECAST_COUNT = 3;
+
+        /// <summary>
+        /// 
+        /// Builds URL for the given location parameter
+        /// 
+        /// </summary>
+        /// <param name="location">Location object for which the URL is to be built.</param>
+        /// <returns>Built URL String</returns>
+        public override string BuildUrl(Location location) {
+            AddUrlOption("lat", location.Latitude.ToString());
+            AddUrlOption("lon", location.Longitude.ToString());
+            AddUrlOption("cnt", FORECAST_COUNT.ToString());
+
+            return this.Build();
+        }
+
+        /// <summary>
+        /// 
+        /// Returns Main Segment of the URL
+        /// 
+        /// </summary>
+        /// <returns>Main Segment of the URL</returns>
+        protected override string GetMainUrlSegment() {
+            return "forecast/daily";
+        }
+    }
+}
