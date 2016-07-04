41a9b5ec8678155664f590d7e9bc255912baa5f0
Amrit Twanabasu <amrit.cas@gmail.com>
2016-07-04 12:54:05 +0545

    Add CameraRenderer to the scene

---
+++ Assets/Scripts/View/CameraRenderer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TheHimalayas.View {
+
+    public class CameraRenderer : MonoBehaviour {
+
+        /// <summary>
+        /// 
+        /// Initializes Camera Renderer
+        /// 
+        /// </summary>
+        public void Initialize() {
+            WebCamDevice[] devices = WebCamTexture.devices;
+
+            string backCamName = "";
+
+            for (int i = 0; i < devices.Length; i++) {
+                if (!devices[i].isFrontFacing) {
+                    backCamName = devices[i].name;
+                }
+            }
+
+            var cameraTexture = new WebCamTexture(backCamName, 1600, 900, 30);
+
+            cameraTexture.Play();
+
+            gameObject.GetComponent<Image>().defaultMaterial.mainTexture = cameraTexture;
+        }
+    }
+}
