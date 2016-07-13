37f77e087a86d05f5e41625b9826f0830001fb0e
Amrit Twanabasu <amrit.cas@gmail.com>
2016-07-13 07:07:36 +0545

    Add day light changing slider

---
+++ Assets/Scripts/Manager/DayLightSystemManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TheHimalayas.Lights;
+
+namespace TheHimalayas.Manager {
+
+    public class DayLightSystemManager : MonoBehaviour {
+
+        /// <summary>
+        /// 
+        /// DayLightSystem instance
+        /// 
+        /// </summary>
+        public DayLightSystem lightSystem;
+
+        /// <summary>
+        /// 
+        /// DayLight System Slider
+        /// 
+        /// </summary>
+        public Slider lightSystemSlider;
+
+        // When the script first enables
+        void OnEnable() {
+            lightSystemSlider.onValueChanged.AddListener(SliderUpdated);
+        }
+
+        // When the script disables
+        void OnDisable() {
+            lightSystemSlider.onValueChanged.RemoveListener(SliderUpdated);
+        }
+
+        // When the script first initializes
+        void Start() {
+            UpdateDayLightSystem();
+        }
+
+        /// <summary>
+        /// 
+        /// Updates day light system based upon current date time
+        /// 
+        /// </summary>
+        private void UpdateDayLightSystem() {
+            DateTime currentTime = Utils.DateTime.GetCurrentDateTimeAtKathmandu();
+
+            lightSystem.UpdateDayLightSystem(currentTime);
+
+            float hourValue = currentTime.Hour + currentTime.Minute / 60f;
+
+            lightSystemSlider.value = hourValue;
+        }
+
+        /// <summary>
+        /// 
+        /// Updates Day Light System from given hour value
+        /// 
+        /// </summary>
+        /// <param name="hourValue">Hour Value of the day</param>
+        private void UpdateDayLightSystem(float hourValue) {
+            int hour = Mathf.FloorToInt(hourValue);
+
+            int min = Mathf.FloorToInt((hourValue - (float) hour) * 60);
+
+            DateTime datetime = new DateTime();
+
+            datetime = datetime.AddHours(hour).AddMinutes(min);
+
+            lightSystem.UpdateDayLightSystem(datetime);
+        }
+
+        /// <summary>
+        /// 
+        /// Called when the slider is updated
+        /// 
+        /// </summary>
+        /// <param name="value">New value that was updated</param>
+        private void SliderUpdated(float value) {
+            UpdateDayLightSystem(value);
+        }
+    }
+}
