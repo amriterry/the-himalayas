fe0f38517239359b847df28f06fcbf1550a82d1a
Amrit Twanabasu <amrit.cas@gmail.com>
2016-07-14 19:37:58 +0545

    Add OneFingerRotationGestureInput and prefab

---
+++ Assets/Scripts/Input/OneFingerRotationGestureInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TheHimalayas.Input {
+
+    public class OneFingerRotationGestureInput : MonoBehaviour, IDragHandler {
+
+        /// <summary>
+        /// 
+        /// Delegate for when the value of the rotation is changed
+        /// 
+        /// </summary>
+        /// <param name="deltaChange">Changed value</param>
+        public delegate void ValueChanged(float deltaChange);
+
+        /// <summary>
+        /// 
+        /// Event when the rotation is detected
+        /// 
+        /// </summary>
+        public event ValueChanged OnRotationDetected;
+
+        // OnDrag Handler
+        public void OnDrag(PointerEventData eventData) {
+            if(OnRotationDetected != null) {
+                OnRotationDetected(eventData.delta.x);
+            }
+        }
+    }
+}
