093f13c1cfc89e79927c039147b8469f7c6f4bfa
Amrit Twanabasu <amrit.cas@gmail.com>
2016-07-14 20:01:48 +0545

    Add ViewModeSelector Panel

---
+++ Assets/Scripts/SceneUtils/ViewModeSelector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using TheHimalayas.Utils;
+using UnityEngine.SceneManagement;
+
+namespace TheHimalayas.SceneUtils {
+
+    public class ViewModeSelector : MonoBehaviour {
+
+        /// <summary>
+        /// 
+        /// Changes to 3D Mode
+        /// 
+        /// </summary>
+        public void ChangeTo3DMode() {
+            ChangeScene(AppScene.MOUNTAIN_3D_SCENE);
+        }
+
+        /// <summary>
+        /// 
+        /// Changes to AR Mode
+        /// 
+        /// </summary>
+        public void ChangeToARMode() {
+            ChangeScene(AppScene.MOUNTAIN_AR_SCENE);
+        }
+
+        /// <summary>
+        /// 
+        /// Changes to VR Mode
+        /// 
+        /// </summary>
+        public void ChangeToVRMode() {
+            ChangeScene(AppScene.MOUNTAIN_VR_SCENE);
+        }
+
+        /// <summary>
+        /// 
+        /// Changes the scene
+        /// 
+        /// </summary>
+        private void ChangeScene(int buildIndex) {
+            if(SceneManager.GetActiveScene().buildIndex != buildIndex) {
+                SceneManager.LoadScene(buildIndex);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/AppScene.cs
@@ -0,0 +1,11 @@
+﻿namespace TheHimalayas.Utils {
+
+    public class AppScene {
+
+        public const int MENU_SCENE = 0;
+        public const int MOUNTAIN_3D_SCENE = 1;
+        public const int MOUNTAIN_AR_SCENE = 2;
+        public const int MOUNTAIN_VR_SCENE = 3;
+        public const int MOUNTAIN_LOCATOR_SCENE = 4;
+    }
+}
