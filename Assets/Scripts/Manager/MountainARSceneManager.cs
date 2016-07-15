fff2eb738f6e9929269d39438db86b2d9397ae66
Amrit Twanabasu <amrit.cas@gmail.com>
2016-07-15 06:56:18 +0545

    Add Mountain to the Image Target

---
+++ Assets/Scripts/Manager/MountainARSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TerrainFactory;
+
+namespace TheHimalayas.Manager { 
+
+    public class MountainARSceneManager : MonoBehaviour {
+
+        /// <summary>
+        /// 
+        /// Image Target to load
+        /// 
+        /// </summary>
+        public Transform imageTarget;
+
+	    // Use this for initialization
+	    void Start () {
+            HeightMapTerrain mountain = GameObject.FindObjectOfType<HeightMapTerrain>();
+
+            if( mountain == null) {
+                Debug.LogError("No HeightMapTerrain to load");
+            } else {
+                mountain.gameObject.transform.SetParent(imageTarget);
+                mountain.gameObject.transform.localScale = new Vector3(1 / 2032f, 1 / 2032f, 1 / 2032f);
+                mountain.gameObject.transform.localPosition = new Vector3(- 1.25f, 0 ,- 1.25f);
+            }
+        }
+	 
+    }
+}
